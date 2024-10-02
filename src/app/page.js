9421f7f82c0d00670bdeba2855c4b2c38fd591e5
Dhananjoy <deydhananjoy5@gmail.com>
2024-10-02 14:07:21 +0530

    added useRouter() to navigate to the products page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 'use client'
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 export default function Home() {
   const [value, setValue] = useState(false);
+  const router = useRouter()
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>Dhananjoy Dey is the name of the developer of this site</h1>
+        <Link href="/products">View Products</Link>
+        <button onClick={() => router.push('products')} >go to products page using use router</button>
       </main>
     </div>
   );
